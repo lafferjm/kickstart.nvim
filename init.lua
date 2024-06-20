3927e01dc35319a6455241f9326b08993c26b148
Justin Lafferty <justin.lafferty@kw.com>
2024-06-20 10:22:32 -0400

    autopairs

--- init.lua
+++ init.lua
@@ -829,7 +829,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.debug',
