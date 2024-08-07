081f9d01272643ef4ebccefb5e99595a7cdcb38d
Justin Lafferty <justin.lafferty@kw.com>
2024-08-07 09:26:11 -0400

    update conform config

--- init.lua
+++ init.lua
@@ -613,7 +613,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
         python = { 'ruff_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
