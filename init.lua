17577226ae294c49790d23765dcb656b5ebb29cc
Justin Lafferty <justin.lafferty@kw.com>
2025-01-18 17:25:27 -0500

    add neo tree

---
+++ init.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+require('config.lazy')
+
