4ba1821c64448d1deafcfce802dd7fbff5bdc980
Justin Lafferty <justin.lafferty@kw.com>
2025-01-19 07:25:29 -0500

    change which-key layout

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
   opts = {
+    preset = 'helix',
     spec = {
       { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
